2ebd898e04569802ed6944f0a24756ecf1b6f179
Ahmedgafsi <ahmed.gafsi.2001@gmail.com>
2024-07-24 15:19:13 +0100

    Committing changes in learningrn

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { useFonts } from 'expo-font';
+import * as SplashScreen from "expo-splash-screen"
+import { useCallback } from 'react';
 export default function App() {
+ 
+  const [fontsLoaded]=useFonts({
+    regular:require('./assets/fonts/Zain-Regular.ttf'),
+    bold:require('./assets/fonts/Zain-Bold.ttf'),
+    extraBold:require('./assets/fonts/Zain-ExtraBold.ttf'),
+    light:require('./assets/fonts/Zain-Light.ttf'),
+    Black:require('./assets/fonts/Zain-Black.ttf'),
+    extraLight:require('./assets/fonts/Zain-ExtraLight.ttf')
+  })
+
+const onLayoutRootView= useCallback(async()=>{
+  if (fontsLoaded){
+   await SplashScreen.hideAsync();
+
+  }
+
+},[fontsLoaded])
+
+   if(!fontsLoaded){
+    return null
+   }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyle}>asslemedd ahmhed</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +43,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle:{
+    fontFamily:"regular",
+    fontSize:30,
+
+  }
 });
+
+
---
+++ navigation/bottomTabNavigateur.jsx
@@ -0,0 +1,15 @@
+import {view,text} from "react-native"
+import React from "react"
+
+
+const BottomTabNav=()=>{
+    return(
+        <view>
+            <text>
+
+            </text>
+        </view>
+    )
+}
+
+export default BottomTabNav
