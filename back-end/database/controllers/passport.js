c8b9ddfc36d7085ea10a382acc83e43ebc6e372f
Ahmedgafsi <ahmed.gafsi.2001@gmail.com>
2024-12-06 17:05:17 +0100

    complete googloAUTH signin

--- back-end/database/controllers/passport.js
+++ back-end/database/controllers/passport.js
@@ -2,20 +2,22 @@ require('dotenv').config()
 const passport = require('passport');
 const crypto = require('crypto');
 const secret = crypto.randomBytes(64).toString('hex');
-console.log(secret);
 
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const db=require("../sequelize/index.js")
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: "615409385557-ileopjc6peho9snf14hpbcipq3l21els.apps.googleusercontent.com",
-      clientSecret: "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",
-      callbackURL: "https://66b9-196-233-246-102.ngrok-free.app/auth/google/callback",
+      clientID: "615409385557-6mjbejkgvmf965pvbcoipbnc3ng8ghpo.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "https://c114-160-156-40-44.ngrok-free.app/auth/google/callback",
       passReqToCallback: true,
     },
     async (req,accessToken, refreshToken, profile, done) => {
+      console.log(profile,"kpfkzefklezezf")
       try {
         const state = JSON.parse(req.query.state || '{}');
         const role = state.role || "buyer"; 
@@ -23,20 +25,27 @@ passport.use(
         let user = await db.users.findOne({ where: { id: profile.id } });
         
         if (!user) {
-         
-          // إنشاء مستخدم جديد إذا لم يكن موجودًا
+          const hashedPassword = await bcrypt.hash("123456", 10); 
           user = await db.users.create({
-            firstname:profile.displayName,
-            password:"123456",
-            role:role,
-            lastname:profile.displayName,
+            firstname: profile.name.givenName,
+            password: hashedPassword,
+            role: role,
+            lastname: profile.name.familyName,
             username: profile.displayName,
-            email: profile.emails[0].value, // تأكد من أن `emails` تحتوي على الإيميل
-            photoDeprofile: profile.photos[0].value, // تأكد من وجود الصور
+            email: profile.emails[0].value,
+            photoDeprofile: profile.photos[0].value,
           });
         }
-        // إرجاع المستخدم
-        return done(null, user);
+
+       
+        const token = jwt.sign(
+          { id: user.id, email: user.email, role: user.role },
+          process.env.SECRET
+       
+        );
+
+        
+        return done(null, { user, token });
       } catch (error) {
         console.error('Error during Google authentication:', error);
         return done(error, null);
@@ -47,12 +56,13 @@ passport.use(
 
 // تخزين واسترجاع المستخدم في الجلسة
 passport.serializeUser((user, done) => {
-  done(null, user.id); // تخزين معرف المستخدم فقط
+  console.log("Serializing userrrrrrrrrrr:", user.user.id);
+  done(null, user.user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await db.users.findByPk(id); // جلب المستخدم بالمعرف
+    const user = await db.users.findByPk(id);
     done(null, user);
   } catch (error) {
     done(error, null);
